f4454ac71de6b9a52a9279a615dc7269d92b2bc9
chaerla <chelgchen@gmail.com>
2023-03-13 03:32:26 +0800

    feat: add Nickname and Point exception

--- src/lib/Exception/Exception.hpp
+++ src/lib/Exception/Exception.hpp
@@ -4,29 +4,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct NumericException : public exception {
-    const char* what() const throw() {
+struct NumericException : public exception
+{
+    const char *what() const throw()
+    {
         return "Numeric input not valid.\nPlease input only integers.";
     }
 };
 
-struct TextFileException : public exception {
-    const char* what() const throw() {
+struct TextFileException : public exception
+{
+    const char *what() const throw()
+    {
         return "TextFile input not valid.\nPlease input correct txt file.";
     }
 };
 
-struct CommandException : public exception {
-    const char* what() const throw() {
+struct CommandException : public exception
+{
+    const char *what() const throw()
+    {
         return "Command input not valid.\nPlease input the right command";
     }
 };
 
-struct StringException : public exception {
-    const char* what() const throw() {
+struct StringException : public exception
+{
+    const char *what() const throw()
+    {
         return "Input is invalid.\n Try input a valid string";
     }
 };
 
+struct NicknameException : public exception
+{
+    const char *what() const throw()
+    {
+        return "No player with that nickname is found.\n";
+    }
+};
+
+struct PointException : public exception
+{
+    const char *what() const throw()
+    {
+        return "The set point is already 1 so it cant be changed.\n";
+    }
+};
 
 #endif
