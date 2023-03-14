ec74367c7dfb9aff87986ff4491e43aeec1aa104
Alexander Jason <alexanderjason526@gmail.com>
2023-03-14 10:14:14 +0700

    feat: fix twopair code

--- src/lib/Valuable/Combo/TwoPair/TwoPair.cpp
+++ src/lib/Valuable/Combo/TwoPair/TwoPair.cpp
@@ -4,5 +4,9 @@ using namespace std;
 TwoPair::TwoPair(Pair firstPair, Pair secondPair):firstPair_(firstPair), secondPair_(secondPair){}
 TwoPair::~TwoPair(){}
 float TwoPair::getValue(){
-    return 4.14 + max(firstPair_.getValue(), secondPair_.getValue());
+    float res = 4.14;   /* maximum pair value */
+
+    res += max(firstPair_.getValue(), secondPair_.getValue());       /* highest pair in the combination */
+    
+    return res;
 }
