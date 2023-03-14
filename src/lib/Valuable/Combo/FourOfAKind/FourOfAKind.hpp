4d08efa47b18575b5279786bbd357187da39ca29
yuujin-Q <92687071+Eugene316@users.noreply.github.com>
2023-03-14 10:53:15 +0700

    feat: Four of A Kind

---
+++ src/lib/Valuable/Combo/FourOfAKind/FourOfAKind.hpp
@@ -0,0 +1,15 @@
+#ifndef _FOUR_OF_A_KIND_HPP_
+#define _FOUR_OF_A_KIND_HPP_
+
+#include "../Combo.hpp"
+
+class FourOfAKind : public Combo {
+    private:
+        vector<ColorCard> quad_;
+    public:
+        FourOfAKind(ColorCard, ColorCard, ColorCard, ColorCard);
+        virtual ~FourOfAKind();
+        float getValue();
+};
+
+#endif
